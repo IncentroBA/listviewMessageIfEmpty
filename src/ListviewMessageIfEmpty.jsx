c8d1ef7c3eca1a6b50e503d369c28a45630fecea
iamrob <rob.duits@incentro.com>
2022-09-29 08:50:42 +0200

    Bugfix
    More stable code

--- src/ListviewMessageIfEmpty.jsx
+++ src/ListviewMessageIfEmpty.jsx
@@ -1,6 +1,5 @@
 import "./ui/ListviewMessageIfEmpty.css";
 import { createElement, useEffect, useRef, useState } from "react";
-import { waitFor } from "./helpers/waitfor";
 
 export default function ListviewMessageIfEmpty({ listviewWidget, emptyWidget, ...rest }) {
     const style = rest.class || "";
@@ -9,34 +8,40 @@ export default function ListviewMessageIfEmpty({ listviewWidget, emptyWidget, ..
     const returnType = { widget: "widget", emptyText: "text" };
     const [displayWidget, setDisplayWidget] = useState(returnType.widget);
     const [displayClass, setDisplayClass] = useState(null);
-    const listviewRef = useRef(null);
+    const config = {
+        // attributes: true,
+        // attributeOldValue: true,
+        childList: true, // This is a must have for the observer with subtree
+        subtree: true // Set to true if changes must also be observed in descendants.
+    };
 
     function callback() {
-        console.info("callback");
-        setTimeout(() => {
-            if (listviewWidgetRef.current.querySelector(".mx-listview-empty")) {
-                setDisplayWidget(returnType.emptyText);
-                setDisplayClass("hidden");
-            } else {
-                setDisplayWidget(returnType.widget);
-                setDisplayClass(null);
-            }
-        }, 10);
+        if (listviewWidgetRef.current.querySelector(".mx-listview-empty")) {
+            setDisplayWidget(returnType.emptyText);
+            setDisplayClass("hidden");
+        } else {
+            setDisplayWidget(returnType.widget);
+            setDisplayClass(null);
+        }
     }
 
     useEffect(() => {
+        const observer = new MutationObserver(() => {
+            observer.disconnect();
+            listviewWidgetRef.current && callback();
+            observer.observe(listviewWidgetRef.current, config);
+        });
+
         if (listviewWidgetRef && listviewWidgetRef.current) {
-            waitFor(".mx-listview-empty", callback, listviewWidgetRef.current);
+            observer.observe(listviewWidgetRef.current, config);
         }
 
-        const listview = listviewWidgetRef.current.querySelector(".mx-listview");
-        listview.ref = listviewRef;
+        return () => observer.disconnect();
     });
 
     return (
         <div className="listview-message-if-empty">
-            <div className={`listview-message-if-empty__widget`} ref={listviewWidgetRef}>
-                {/* <div className={`listview-message-if-empty__widget ${displayClass}`} ref={listviewWidgetRef}> */}
+            <div className={`listview-message-if-empty__widget ${displayClass}`} ref={listviewWidgetRef}>
                 {listviewWidget}
             </div>
             {displayWidget === returnType.emptyText && (
